7ec61b9326eecfba2b09a99c380a75ae981c6854
trafalgarandre <trafalgarandre@gmail.com>
2019-03-27 15:01:43 +0800

    Fix bug and decrease number of var

--- BayesianToCNF.cpp
+++ BayesianToCNF.cpp
@@ -1,26 +1,36 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <algorithm>
 #include <stack>
 #include <unordered_map>
 #include <stdio.h>
 #include <string.h>
+#include <string>
+#include <math.h>
 
 using namespace std;
 
 class CNFFormula {
 public:
+    // CNF form, vector of clauses
     vector<vector<int>> formula;
     CNFFormula() {
         formula.clear();
+    }
+
+    CNFFormula(vector<vector<int>> f) {
+        formula = f;
     }
 
     int getNumClause() {
         return formula.size();
     }
 
-    CNFFormula(vector<vector<int>> f) {
-        formula = f;
+    void andFormula(CNFFormula f) {
+        for (vector<int> vi: f.formula) {
+            addClause(vi);
+        }
     }
 
     void addClause(vector<int> clause) {
@@ -35,12 +45,6 @@ public:
             for (int i = 0; i < formula.size(); i++) {
                 formula[i].push_back(v);
             }
-        }
-    }
-
-    void andFormula(CNFFormula f) {
-        for (vector<int> vi: f.formula) {
-            addClause(vi);
         }
     }
 
@@ -94,6 +98,7 @@ public:
 
     CNFFormula toCNFFormula() {
         // the left in the format a ^ b ^ c ^ d
+        // the right supposes to only have 1 variable
         CNFFormula result;
         for (vector<int> vi: right.formula) {
             vector<int> temp = vi;
@@ -113,12 +118,14 @@ public:
     int reduce_factor; // use to extend m' -> m, (x V ()) ^ *()
     int seed;
     int length;
+    double c;
     ChainFormula chain;
     CNFFormula f;
     Frac(int _numerator, int _denominator) {
         numerator = _numerator;
-        denominator = _denominator;
         reduce_factor = 0;
+        denominator = pow(2, ceil(log2(_denominator)));
+        c = ((double)denominator) / _denominator;
     }
 
     void toLowestForm() {
@@ -187,6 +194,10 @@ public:
     int returnSeed() {
         return seed;
     }
+
+    double getC() {
+        return c;
+    }
 };
 
 class Bayes {
@@ -195,11 +206,13 @@ public:
     vector<int> cardinality;
     vector<vector<int>> parent;
     vector<vector<double>> cpt;
+    vector<int> cpt_denominator;
     unordered_map<string, int> hmap;
     int seed = 1;
     CNFFormula formula;
+    double C = 1;
 
-    void solve() {
+    void createKB() {
         formula = generateIndicator();
         CNFFormula par = generateParameter();
         formula.andFormula(par);
@@ -216,14 +229,14 @@ public:
                 seed++;
             }
             result.addClause(clause);
+            vector<int> only1Clause;
             for (int j = 1; j < cardinality[i]; j++) {
                 for (int k = 0; k < j; k++) {
-                    clause.clear();
                     string key1 = to_string(i) + " " + to_string(j);
                     string key2 = to_string(i) + " " + to_string(k);
-                    clause.push_back(-(hmap.at(key1)));
-                    clause.push_back(-(hmap.at(key2)));
-                    result.addClause(clause);
+                    only1Clause.push_back(-(hmap.at(key1)));
+                    only1Clause.push_back(-(hmap.at(key2)));
+                    result.addClause(only1Clause);
                 }
             }
         }
@@ -233,12 +246,15 @@ public:
     CNFFormula generateParameter() {
         CNFFormula result;
         for (int i = 0; i < num_nodes; i++) {
+            int start_seed = seed;
+            int end_seed = 0;
             vector<int> vi = parent[i];
             vi.push_back(i);
             int pos = vi.size() - 1;
             int arr[vi.size()];
             memset(arr, 0, sizeof(arr));
             int index = 0;
+            bool getC = false;
             while (pos >= 0) {
                 CNFFormula left;
                 CNFFormula right;
@@ -251,87 +267,135 @@ public:
                         left.and1Var(hmap.at(key));
                     }
                 }
-                Frac frac((int)(cpt[i][index] * 1000000), 1000000);
+                Frac frac((int)(cpt[i][index] * cpt_denominator[i]), cpt_denominator[i]);
                 //Frac frac((int)(cpt[i][index] * 10), 4);
-
-                frac.constructCNFFormula(seed);
-                seed = frac.returnSeed();
+                if (!getC) {
+                    getC = true;
+                    C *= frac.getC();
+                }
+                frac.constructCNFFormula(start_seed);
+                int temp_seed = frac.returnSeed();
+                if (temp_seed > end_seed) end_seed = temp_seed;
                 ImplyFormula imply(&left, &frac.f);
                 result.andFormula(imply.toCNFFormula());
+                bool step = false;
                 while (arr[pos] == cardinality[vi[pos]] - 1) {
                     pos--;
+                    step = true;
                 }
                 index++;
-                if (pos >= 0) {
-                    arr[pos]++;
-                }
-                for (int j = pos + 1; j < vi.size(); j++) {
-                    arr[j] = 0;
+                if (pos >= 0 && !step) {
+                    if (!step) {
+                        arr[pos]++;
+                    } else {
+                        for (int j = pos + 1; j < vi.size(); j++) {
+                            arr[j] = 0;
+                        }
+                    }
                 }
             }
+            seed = end_seed;
         }
         return result;
+    }
+
+    double getC() {
+        return C;
+    }
+
+    void addEvid(int var, int val) {
+        formula.addClause({hmap.at(to_string(var) + " " + to_string(val))});
     }
 };
 
 Bayes bayes;
-const Frac C = Frac(16777216, 10000000);
+//const Frac C = Frac(16777216, 10000000);
 
 int main()
 {
-    freopen("in2.txt", "r", stdin);
+    ofstream cnfFile, CFile;
+    ifstream bayesFile, evidFile;
+    bayesFile.open("in2.txt");
+    cnfFile.open ("cnf.txt");
+    CFile.open("c.txt");
+    evidFile.open("evid.txt");
+    //freopen("in2.txt", "r", stdin);
     //Start inputing
     string type;
-    cin >> type;
+    bayesFile >> type;
     int ignore;
     //cout << type << endl;
     if (type != "BAYES") {
         throw "Not Bayes";
     }
 
-    cin >> bayes.num_nodes;
+    bayesFile >> bayes.num_nodes;
 
     for (int i = 0; i < bayes.num_nodes; i++) {
         int temp;
-        cin >> temp;
+        bayesFile >> temp;
         bayes.cardinality.push_back(temp);
         bayes.parent.push_back({});
         bayes.cpt.push_back({});
     }
 
-    cin >> ignore;
+    bayesFile >> ignore;
 
     for (int i = 0; i < bayes.num_nodes; i++) {
         int cpt_nums;
-        cin >> cpt_nums;
+        bayesFile >> cpt_nums;
         for (int j = 0; j < cpt_nums - 1; j++) {
             int temp;
-            cin >> temp;
+            bayesFile >> temp;
             bayes.parent[i].push_back(temp);
         }
-        cin >> ignore;
+        bayesFile >> ignore;
     }
 
     for (int i = 0; i < bayes.num_nodes; i++) {
         int num_network_params;
-        cin >> num_network_params;
+        bayesFile >> num_network_params;
+        int den_num = 0;
         for (int j = 0; j < num_network_params; j++) {
+            string temp_str;
             double temp;
-            cin >> temp;
-            bayes.cpt[i].push_back(temp);
+            bayesFile >> temp_str;
+            if (!temp_str.empty()) {
+                temp = stod(temp_str);
+                bayes.cpt[i].push_back(temp);
+                int temp_den_num = temp_str.length() - temp_str.find('.');
+                den_num = max(den_num, temp_den_num);
+            }
         }
+        int den_value = 1;
+        for (int i = 0; i < den_num; i++) {
+            den_value *= 10;
+        }
+        bayes.cpt_denominator.push_back(den_value);
     }
     // End inputing
 
-    bayes.solve();
+    bayes.createKB();
 
-    cout << "p cnf " << bayes.seed << " " << bayes.formula.getNumClause() << "\n";
-    for (vector<int> vi: bayes.formula.formula) {
-        for (int i: vi) {
-            cout << i << " ";
-        }
-        cout << "0\n";
+    int num_var;
+    evidFile >> num_var;
+    for (int i = 0; i < num_var; i++) {
+        int var_num, var_val;
+        evidFile >> var_num >> var_val;
+        bayes.addEvid(var_num, var_val);
     }
 
+    cnfFile << "p cnf " << bayes.seed << " " << bayes.formula.getNumClause() << "\n";
+    for (vector<int> vi: bayes.formula.formula) {
+        for (int i: vi) {
+            cnfFile << i << " ";
+        }
+        cnfFile << "0\n";
+    }
+
+    CFile << bayes.C;
+    cnfFile.close();
+    CFile.close();
+    bayesFile.close();
     return 0;
 }
